97103d3aef24d0dc42824990fdd9dea1b990bd39
Anwar Hossain <anwar.sarker@bjitgroup.com>
2024-01-25 16:48:28 +0600

    Delete unused files and dependencies

---
+++ laravel-10-crud/app/Http/Controllers/ProductController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+        //return as json
+        return response()->json([
+            'success' => true,
+            'message' => 'Product List',
+            'data' => $products
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+
+        $product = Product::create($request->all());
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Product created successfully',
+            'data' => $product
+        ], Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'Product retrieved successfully',
+            'data' => $product
+        ], Response::HTTP_OK);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product): JsonResponse
+    {
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+
+        $product->update($request->all());
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Product updated successfully',
+            'data' => $product
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product): JsonResponse
+    {
+        $product->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Product deleted successfully',
+        ], Response::HTTP_OK);
+    }
+}
