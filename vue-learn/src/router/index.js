48e5fe5ca14b17981a2d9e6b84062c138c561771
Restyled.io <commits@restyled.io>
2024-01-26 05:43:06 +0000

    Restyled by clang-format

--- vue-learn/src/router/index.js
+++ vue-learn/src/router/index.js
@@ -1,27 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+  history : createWebHistory(import.meta.env.BASE_URL),
+  routes : [
     {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/Home/Index.vue')
+      path : '/',
+      name : 'home',
+      component : () => import('../views/Home/Index.vue')
     },
     {
-      path: '/products',
-      name: 'ProductsIndex',
-      component: () => import('../views/Products/Index.vue')
+      path : '/products',
+      name : 'ProductsIndex',
+      component : () => import('../views/Products/Index.vue')
     },
     {
-      path: '/products/create',
-      name: 'ProductsCreate',
-      component: () => import('../views/Products/Create.vue')
+      path : '/products/create',
+      name : 'ProductsCreate',
+      component : () => import('../views/Products/Create.vue')
     },
     {
-      path: '/products/:id/edit',
-      name: 'ProductsEdit',
-      component: () => import('../views/Products/Edit.vue')
+      path : '/products/:id/edit',
+      name : 'ProductsEdit',
+      component : () => import('../views/Products/Edit.vue')
     }
   ]
 })
